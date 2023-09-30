dd08c92475bf43d4c3cf13a4ace69d46b53de634
choiseonkang <com1368@naver.com>
2023-09-30 10:48:43 +0900

    feat: 택시기사 조회 service 테스트 구현

---
+++ src/test/kotlin/hanghae/four/taxiservice/unit/domain/driver/DriverServiceTest.kt
@@ -0,0 +1,26 @@
+package hanghae.four.taxiservice.unit.domain.driver
+
+import hanghae.four.taxiservice.domain.driver.DriverService
+import hanghae.four.taxiservice.unit.infrastructures.driver.FakeDriverRepository
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class DriverServiceTest {
+    private lateinit var driverService: DriverService
+
+    private lateinit var driverReader: FakeDriverRepository
+
+    @BeforeEach
+    fun setup() {
+        driverReader = FakeDriverRepository()
+        driverService = DriverService(driverReader)
+    }
+
+    @Test
+    fun `택시기사 id로 조회 성공`() {
+        val driver = driverService.getDriver(1L)
+
+        assertThat(driver.id).isEqualTo(1L)
+    }
+}
