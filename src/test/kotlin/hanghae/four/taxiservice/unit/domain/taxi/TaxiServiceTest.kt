18ff988d01dffd255e95b1b7c5838e76d84db45b
choiseonkang <com1368@naver.com>
2023-09-30 15:48:44 +0900

    feat: 택시번호 중복 등록 service 테스트 구현

--- src/test/kotlin/hanghae/four/taxiservice/unit/domain/taxi/TaxiServiceTest.kt
+++ src/test/kotlin/hanghae/four/taxiservice/unit/domain/taxi/TaxiServiceTest.kt
@@ -6,8 +6,10 @@ import hanghae.four.taxiservice.domain.taxi.TaxiService
 import hanghae.four.taxiservice.domain.taxi.TaxiStore
 import hanghae.four.taxiservice.unit.infrastructures.taxi.FakeTaxiStore
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.springframework.dao.DataIntegrityViolationException
 
 internal class TaxiServiceTest {
     private lateinit var taxiService: TaxiService
@@ -26,5 +28,16 @@ internal class TaxiServiceTest {
         val taxiId = taxiService.register(request)
 
         assertThat(taxiId).isEqualTo(1L)
+    }
+
+    @Test
+    fun `택시번호 중복시 에러`() {
+        taxiStore.store(Taxi(1L, Taxi.Type.NORMAL, 1234, Taxi.Status.CLOSED))
+
+        val request = TaxiCommand.RegisterTaxi(1L, type = Taxi.Type.NORMAL, 1234)
+
+        assertThatThrownBy{taxiService.register(request)}
+            .isInstanceOf(DataIntegrityViolationException::class.java)
+            .hasMessage("중복된 택시 번호가 있습니다.")
     }
 }
