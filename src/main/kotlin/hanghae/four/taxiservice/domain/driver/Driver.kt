f138a22b3e927031b1aa54583ec89de34f2d3842
lass9436 <lass9436@gmail.com>
2023-10-04 19:43:20 +0900

    임시 오류 해결

--- src/main/kotlin/hanghae/four/taxiservice/domain/driver/Driver.kt
+++ src/main/kotlin/hanghae/four/taxiservice/domain/driver/Driver.kt
@@ -1,10 +1,12 @@
 package hanghae.four.taxiservice.domain.driver
 
 import javax.persistence.Entity
+import javax.persistence.Column
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
 import javax.persistence.Table
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "driver")
