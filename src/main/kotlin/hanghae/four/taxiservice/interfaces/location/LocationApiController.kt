51804303a33a902513313e04d6fd3493e1621249
lass9436 <lass9436@gmail.com>
2023-10-05 19:36:31 +0900

    feat: location add 성공 기능 구현

--- src/main/kotlin/hanghae/four/taxiservice/interfaces/location/LocationApiController.kt
+++ src/main/kotlin/hanghae/four/taxiservice/interfaces/location/LocationApiController.kt
@@ -1,19 +1,20 @@
 package hanghae.four.taxiservice.interfaces.location
 
+import hanghae.four.taxiservice.domain.location.LocationService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class LocationApiController {
+class LocationApiController(
+    private val locationService: LocationService,
+    private val addMapper: addApiMapper,
+) {
 
     @PostMapping("/api/v1/location/add")
-    fun addLocation(): ResponseEntity<*> {
-        val mockResponse = AddResponse(
-            clientId = 1L,
-            locationId = 1L,
-        )
-
-        return ResponseEntity.ok(mockResponse)
+    fun addLocation(@RequestBody addRequest: AddRequest): ResponseEntity<AddResponse> {
+        val addResult = locationService.add(addMapper.mapToAddCommand(addRequest))
+        return ResponseEntity.ok(addMapper.mapToResponse(addResult))
     }
 }
