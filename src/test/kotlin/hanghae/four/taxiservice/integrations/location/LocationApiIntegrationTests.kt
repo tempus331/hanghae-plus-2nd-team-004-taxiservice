37af389e488fff126daee25988d946874100c743
lass9436 <lass9436@gmail.com>
2023-10-05 19:36:44 +0900

    test: location add test 수정

--- src/test/kotlin/hanghae/four/taxiservice/integrations/location/LocationApiIntegrationTests.kt
+++ src/test/kotlin/hanghae/four/taxiservice/integrations/location/LocationApiIntegrationTests.kt
@@ -3,6 +3,8 @@ package hanghae.four.taxiservice.integrations.location
 import hanghae.four.taxiservice.integrations.AbstractIntegrationTests
 import hanghae.four.taxiservice.interfaces.location.AddRequest
 import hanghae.four.taxiservice.interfaces.location.AddResponse
+import org.hamcrest.Matchers
+import org.hamcrest.Matchers.equalTo
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
@@ -14,7 +16,7 @@ class LocationApiIntegrationTests : AbstractIntegrationTests() {
     @Test
     fun `위치를 즐겨찾기로 등록한다`() {
         val mockRequest = AddRequest(
-            clientId = 1L,
+            clientId = 100L,
             country = "",
             city = "",
             placeName = "",
@@ -22,17 +24,13 @@ class LocationApiIntegrationTests : AbstractIntegrationTests() {
             longitude = 127.044529,
         )
 
-        val mockResponse = AddResponse(
-            clientId = 1L,
-            locationId = 1L,
-        )
-
         mockMvc.post("/api/v1/location/add") {
             contentType = MediaType.APPLICATION_JSON
             content = objectMapper.writeValueAsString(mockRequest)
         }.andExpect {
             status { isOk() }
-            content { json(objectMapper.writeValueAsString(mockResponse))}
+            jsonPath("$.locationId", Matchers.notNullValue())
+            jsonPath("$.clientId", equalTo(100)) // clientId가 1인지 검증
         }
     }
 }
