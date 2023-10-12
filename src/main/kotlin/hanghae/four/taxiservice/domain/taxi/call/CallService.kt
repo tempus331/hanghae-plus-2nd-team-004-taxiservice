faddc86002fb5a2e21ea26eec71106181c05633c
choiseonkang <com1368@naver.com>
2023-10-12 23:42:18 +0900

    develop merge

--- src/main/kotlin/hanghae/four/taxiservice/domain/taxi/call/CallService.kt
+++ src/main/kotlin/hanghae/four/taxiservice/domain/taxi/call/CallService.kt
@@ -3,18 +3,43 @@ package hanghae.four.taxiservice.domain.taxi.call
 import hanghae.four.taxiservice.domain.taxi.TaxiAllocator
 import hanghae.four.taxiservice.domain.taxi.TaxiFinder
 import hanghae.four.taxiservice.domain.taxi.call.dispatch.toCallResult
+import hanghae.four.taxiservice.infrastructures.taxi.call.exception.NotExistsCallableTaxiException
+import org.springframework.retry.annotation.Backoff
+import org.springframework.retry.annotation.Recover
+import org.springframework.retry.annotation.Retryable
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 
 @Service
-@Transactional
 class CallService(
     val taxiFinder: TaxiFinder,
     val taxiAllocator: TaxiAllocator,
+    val callReader: CallReader,
+    val fareCalculator: FareCalculator,
 ) {
+
+    @Retryable(
+        value = [NotExistsCallableTaxiException::class],
+        maxAttempts = 3,
+        backoff = Backoff(delay = 3000L)
+    )
     fun call(callCommand: CallCommand): CallResult {
         // todo: locking 조회 시점엔 택시가 running 상태가 아니어도 최종 시점엔 running 상태일 수 있음
         val usableTaxis = taxiFinder.findUsableTaxis(callCommand.type)
         return taxiAllocator.execute(usableTaxis, callCommand).toCallResult()
+    }
+
+    fun getCallDetailInfo(callId: Long): CallDetailInfo = with(callReader.getById(callId)) {
+        // todo: user 정보는 추후 구현
+        CallDetailInfo(
+            origin = this.origin,
+            destination = this.destination,
+            fare = fareCalculator.calculate(origin, destination)
+        )
+    }
+
+    @Recover
+    fun recover(): CallResult? {
+        throw NotExistsCallableTaxiException()
+        return null
     }
 }
--- src/test/kotlin/hanghae/four/taxiservice/unit/domain/call/CallServiceTest.kt
+++ src/test/kotlin/hanghae/four/taxiservice/unit/domain/call/CallServiceTest.kt
@@ -2,11 +2,16 @@ package hanghae.four.taxiservice.unit.domain.call
 
 import hanghae.four.taxiservice.domain.taxi.TaxiAllocator
 import hanghae.four.taxiservice.domain.taxi.TaxiFinder
+import hanghae.four.taxiservice.domain.taxi.call.Call
 import hanghae.four.taxiservice.domain.taxi.call.CallCommand
+import hanghae.four.taxiservice.domain.taxi.call.CallDetailInfo
 import hanghae.four.taxiservice.domain.taxi.call.CallService
+import hanghae.four.taxiservice.domain.taxi.call.FareCalculator
 import hanghae.four.taxiservice.domain.taxi.call.dispatch.DispatchResult
 import hanghae.four.taxiservice.domain.taxi.call.dispatch.toCallResult
+import hanghae.four.taxiservice.infrastructures.taxi.call.exception.CallNotFoundException
 import hanghae.four.taxiservice.unit.domain.call.fakes.FakeTaxiFinder
+import hanghae.four.taxiservice.unit.infrastructures.call.FakeCallRepository
 import io.mockk.every
 import io.mockk.mockk
 import org.assertj.core.api.Assertions.assertThat
@@ -24,11 +29,13 @@ internal class CallServiceTest {
 
     private val taxiAllocator: TaxiAllocator = mockk()
     private val taxiFinder: TaxiFinder = FakeTaxiFinder()
+    private val callRepository = FakeCallRepository()
+    private val fareCalculator: FareCalculator = mockk()
     private lateinit var sut: CallService
 
     @BeforeEach
     fun setUp() {
-        sut = CallService(taxiFinder, taxiAllocator)
+        sut = CallService(taxiFinder, taxiAllocator, callRepository, fareCalculator)
     }
 
     @DisplayName("call 메서드는")
@@ -82,6 +89,56 @@ internal class CallServiceTest {
                     )
                 } throws IllegalArgumentException("호출 가능한 택시가 없습니다.") // 익셉션 발생
                 assertThrows<IllegalArgumentException> { sut.call(callCommand) }
+            }
+        }
+    }
+
+    @DisplayName("getDetailInfo 메서드는")
+    @Nested
+    inner class Describe_of_getDetailInfo {
+
+        @DisplayName("유효한 Call Id를 받으면,")
+        @Nested
+        inner class Context_of_valid_call_id {
+
+            @DisplayName("호출 상세 정보를 반환한다")
+            @Test
+            fun it_returns_call_detail_info() {
+                val mockDetailInfo = CallDetailInfo(
+                    origin = "서울시 강남구",
+                    destination = "서울시 강북구",
+                    fare = 10000
+                )
+
+                callRepository.store(
+                    Call(
+                        id = 1L,
+                        userId = 1L,
+                        callNumber = "1234",
+                        origin = "서울시 강남구",
+                        destination = "서울시 강북구",
+                        status = Call.CallStatus.RUNNING,
+                        taxiId = 1L
+                    )
+                )
+
+                every { fareCalculator.calculate(any(), any()) } returns 10000
+
+                val res = sut.getCallDetailInfo(1L)
+                assertThat(res).isEqualTo(mockDetailInfo)
+            }
+        }
+
+        @DisplayName("존재하지 않는 Call Id를 받으면,")
+        @Nested
+        inner class Context_of_invalid_call_id {
+
+            @DisplayName("예외가 발생한다.")
+            @Test
+            fun it_returns_call_detail_info() {
+                assertThrows<CallNotFoundException> {
+                    sut.getCallDetailInfo(9999L)
+                }
             }
         }
     }
