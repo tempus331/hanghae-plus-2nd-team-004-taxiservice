090693a069c09358caaf5b38a658578c422ae7cf
choiseonkang <com1368@naver.com>
2023-10-21 21:15:25 +0900

    feat : 택시 등록 코드 리뷰 리팩토링

--- src/main/kotlin/hanghae/four/taxiservice/domain/taxi/TaxiService.kt
+++ src/main/kotlin/hanghae/four/taxiservice/domain/taxi/TaxiService.kt
@@ -1,23 +1,19 @@
 package hanghae.four.taxiservice.domain.taxi
 
-import hanghae.four.taxiservice.domain.taxi.call.driver.DriverReader
 import org.springframework.stereotype.Service
 import javax.persistence.EntityExistsException
 
 @Service
 class TaxiService(
-    private val driverReader: DriverReader,
     private val taxiStore: TaxiStore,
     private val taxiReader: TaxiReader,
 ) {
-    fun register(request: RegisterTaxi): Long {
-        driverReader.getDriver(request.driverId)
-
-        if (taxiReader.existsBy(request.number)) {
+    fun register(command: RegisterTaxi): Long {
+        if (taxiReader.existsBy(command.number)) {
             throw EntityExistsException("중복된 택시 번호가 있습니다.")
         }
 
-        val taxi = taxiStore.store(request.toEntity())
+        val taxi = taxiStore.store(command.toEntity())
         return requireNotNull(taxi.id)
     }
 }
